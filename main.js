7016734a6bc38b1a743840f2bbeae983675d0382
Fachry <151fachry@gmail.com>
2024-08-12 21:13:58 +0700

    style: change API and modify display on chartjs

--- main.js
+++ main.js
@@ -1,229 +1,239 @@
 /*jshint esversion:6*/
 
 $(function () {
-    const video = $("video")[0];
+  const video = $("video")[0];
 
-    var model;
-    var cameraMode = "environment"; // or "user"
+  var model;
+  var cameraMode = "environment"; // or "user"
 
-    const startVideoStreamPromise = navigator.mediaDevices
-        .getUserMedia({
-            audio: false,
-            video: {
-                facingMode: cameraMode
-            }
-        })
-        .then(function (stream) {
-            return new Promise(function (resolve) {
-                video.srcObject = stream;
-                video.onloadeddata = function () {
-                    video.play();
-                    resolve();
-                };
-            });
-        });
-
-    var publishable_key = "rf_nOGdFfQSHMMzs9W04v0LrsYa0vu1";
-    var toLoad = {
-        model: "face-detection-f7nvo",
-        version: 1
-    };
-
-    const loadModelPromise = new Promise(function (resolve, reject) {
-        roboflow
-            .auth({
-                publishable_key: publishable_key
-            })
-            .load(toLoad)
-            .then(function (m) {
-                model = m;
-                resolve();
-            });
-    });
-
-    Promise.all([startVideoStreamPromise, loadModelPromise]).then(function () {
-        $("body").removeClass("loading");
-        resizeCanvas();
-        detectFrame();
-    });
-
-    var canvas, ctx;
-    const font = "16px sans-serif";
-
-    function videoDimensions(video) {
-        // Ratio of the video's intrisic dimensions
-        var videoRatio = video.videoWidth / video.videoHeight;
-
-        // The width and height of the video element
-        var width = video.offsetWidth,
-            height = video.offsetHeight;
-
-        // The ratio of the element's width to its height
-        var elementRatio = width / height;
-
-        // If the video element is short and wide
-        if (elementRatio > videoRatio) {
-            width = height * videoRatio;
-        } else {
-            // It must be tall and thin, or exactly equal to the original ratio
-            height = width / videoRatio;
-        }
-
-        return {
-            width: width,
-            height: height
+  const startVideoStreamPromise = navigator.mediaDevices
+    .getUserMedia({
+      audio: false,
+      video: {
+        facingMode: cameraMode,
+      },
+    })
+    .then(function (stream) {
+      return new Promise(function (resolve) {
+        video.srcObject = stream;
+        video.onloadeddata = function () {
+          video.play();
+          resolve();
         };
+      });
+    });
+
+  var publishable_key = "rf_WoSRlzWhvBbm1qLcePaq";
+  var toLoad = {
+    model: "detection-face-2",
+    version: 1,
+  };
+
+  const loadModelPromise = new Promise(function (resolve, reject) {
+    roboflow
+      .auth({
+        publishable_key: publishable_key,
+      })
+      .load(toLoad)
+      .then(function (m) {
+        model = m;
+        resolve();
+      });
+  });
+
+  Promise.all([startVideoStreamPromise, loadModelPromise]).then(function () {
+    $("body").removeClass("loading");
+    resizeCanvas();
+    detectFrame();
+  });
+
+  var canvas, ctx;
+  const font = "16px sans-serif";
+
+  function videoDimensions(video) {
+    // Ratio of the video's intrisic dimensions
+    var videoRatio = video.videoWidth / video.videoHeight;
+
+    // The width and height of the video element
+    var width = video.offsetWidth,
+      height = video.offsetHeight;
+
+    // The ratio of the element's width to its height
+    var elementRatio = width / height;
+
+    // If the video element is short and wide
+    if (elementRatio > videoRatio) {
+      width = height * videoRatio;
+    } else {
+      // It must be tall and thin, or exactly equal to the original ratio
+      height = width / videoRatio;
     }
 
-    $(window).resize(function () {
-        resizeCanvas();
+    return {
+      width: width,
+      height: height,
+    };
+  }
+
+  $(window).resize(function () {
+    resizeCanvas();
+  });
+
+  const resizeCanvas = function () {
+    $("canvas").remove();
+
+    canvas = $("<canvas/>");
+
+    ctx = canvas[0].getContext("2d");
+
+    var dimensions = videoDimensions(video);
+
+    console.log(
+      video.videoWidth,
+      video.videoHeight,
+      video.offsetWidth,
+      video.offsetHeight,
+      dimensions
+    );
+
+    canvas[0].width = video.videoWidth;
+    canvas[0].height = video.videoHeight;
+
+    canvas.css({
+      width: dimensions.width,
+      height: dimensions.height,
+      left: ($(window).width() - dimensions.width) / 2,
+      top: ($(window).height() - dimensions.height) / 2,
     });
 
-    const resizeCanvas = function () {
-        $("canvas").remove();
+    $("body").append(canvas);
+  };
 
-        canvas = $("<canvas/>");
+  const renderPredictions = function (predictions) {
+    var dimensions = videoDimensions(video);
 
-        ctx = canvas[0].getContext("2d");
+    var scale = 1;
 
-        var dimensions = videoDimensions(video);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        console.log(
-            video.videoWidth,
-            video.videoHeight,
-            video.offsetWidth,
-            video.offsetHeight,
-            dimensions
-        );
+    predictions.forEach(function (prediction) {
+      const x = prediction.bbox.x;
+      const y = prediction.bbox.y;
 
-        canvas[0].width = video.videoWidth;
-        canvas[0].height = video.videoHeight;
+      const width = prediction.bbox.width;
+      const height = prediction.bbox.height;
 
-        canvas.css({
-            width: dimensions.width,
-            height: dimensions.height,
-            left: ($(window).width() - dimensions.width) / 2,
-            top: ($(window).height() - dimensions.height) / 2
+      ctx.strokeStyle = prediction.color;
+      ctx.lineWidth = 4;
+      ctx.strokeRect(
+        (x - width / 2) / scale,
+        (y - height / 2) / scale,
+        width / scale,
+        height / scale
+      );
+
+      // Draw the label background.
+      ctx.fillStyle = prediction.color;
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(
+        (x - width / 2) / scale,
+        (y - height / 2) / scale,
+        textWidth + 8,
+        textHeight + 4
+      );
+    });
+
+    predictions.forEach(function (prediction) {
+      const x = prediction.bbox.x;
+      const y = prediction.bbox.y;
+
+      const width = prediction.bbox.width;
+      const height = prediction.bbox.height;
+
+      // Draw the text last to ensure it's on top.
+      ctx.font = font;
+      ctx.textBaseline = "top";
+      ctx.fillStyle = "#FF0000";
+      ctx.fillText(
+        prediction.class + ": " + Math.round(prediction.confidence * 100) + "%",
+        (x - width / 2) / scale + 4,
+        (y - height / 2) / scale + 1
+      );
+    });
+    let labels = ["Focus", "Less-Focus", "Not-Focus"];
+    localStorage.setItem("labels", JSON.stringify(labels));
+
+    //Get existing data from local storage
+    let existingData = JSON.parse(localStorage.getItem("data")) || [];
+
+    // Map new predictions to the required format
+    let newData = predictions.map((prediction) => {
+      return {
+        prediction: prediction.class,
+        confidence: prediction.confidence,
+      };
+    });
+
+    // Append new data to existing data
+    let combinedData = existingData.concat(newData);
+
+    // Check if the length of the combined data is less than 50
+    if (combinedData.length <= 40) {
+      // Save combined data back to local storage
+      localStorage.setItem("data", JSON.stringify(combinedData));
+    } else {
+      console.log("Data limit reached. Not saving new predictions.");
+    }
+  };
+
+  var prevTime;
+  var pastFrameTimes = [];
+  const detectFrame = function () {
+    if (!model) return requestAnimationFrame(detectFrame);
+
+    model
+      .detect(video)
+      .then(function (predictions) {
+        requestAnimationFrame(detectFrame);
+        renderPredictions(predictions);
+
+        predictions.forEach((prediction) => {
+          console.log(
+            "DETECTED",
+            "Class: ",
+            prediction.class,
+            "Confidence: ",
+            prediction.confidence,
+            "Box: ",
+            prediction.bbox
+          );
         });
 
-        $("body").append(canvas);
-    };
+        if (prevTime) {
+          pastFrameTimes.push(Date.now() - prevTime);
+          if (pastFrameTimes.length > 30) pastFrameTimes.shift();
 
-    const renderPredictions = function (predictions) {
-        var dimensions = videoDimensions(video);
-        
-        var scale = 1;
-        
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+          var total = 0;
+          _.each(pastFrameTimes, function (t) {
+            total += t / 1000;
+          });
 
-        predictions.forEach(function (prediction) {
-            const x = prediction.bbox.x;
-            const y = prediction.bbox.y;
+          var fps = pastFrameTimes.length / total;
+          $("#fps").text(Math.round(fps));
+        }
+        prevTime = Date.now();
+      })
+      .catch(function (e) {
+        console.log("CAUGHT", e);
+        requestAnimationFrame(detectFrame);
+      });
+  };
+  // This should be done after you get the predictions
+  let results = predictions.map((prediction) => ({
+    class: prediction.class,
+    confidence: Math.round(prediction.confidence * 100),
+  }));
 
-            const width = prediction.bbox.width;
-            const height = prediction.bbox.height;
-            
-            ctx.strokeStyle = prediction.color;
-            ctx.lineWidth = 4;
-            ctx.strokeRect(
-                (x - width / 2) / scale,
-                (y - height / 2) / scale,
-                width / scale,
-                height / scale
-            );
-
-            // Draw the label background.
-            ctx.fillStyle = prediction.color;
-            const textWidth = ctx.measureText(prediction.class).width;
-            const textHeight = parseInt(font, 10); // base 10
-            ctx.fillRect(
-                (x - width / 2) / scale,
-                (y - height / 2) / scale,
-                textWidth + 8,
-                textHeight + 4
-                );
-            });
-            
-            predictions.forEach(function (prediction) {
-            const x = prediction.bbox.x;
-            const y = prediction.bbox.y;
-
-            const width = prediction.bbox.width;
-            const height = prediction.bbox.height;
-
-            // Draw the text last to ensure it's on top.
-            ctx.font = font;
-            ctx.textBaseline = "top";
-            ctx.fillStyle = "#FF0000";
-            ctx.fillText(
-                prediction.class + ': ' + Math.round(prediction.confidence * 100) + '%',
-                (x - width / 2) / scale + 4,
-                (y - height / 2) / scale + 1
-                );
-            });
-                let labels = ["fokus", "kurang-fokus", "tidak-fokus"];
-                localStorage.setItem('labels', JSON.stringify(labels));
-                
-                //Get existing data from local storage
-                let existingData = JSON.parse(localStorage.getItem('data')) || [];
-
-                // Map new predictions to the required format
-                let newData = predictions.map(prediction => {
-                    return { prediction: prediction.class, confidence: prediction.confidence };
-                });
-
-                // Append new data to existing data
-                let combinedData = existingData.concat(newData);
-
-              // Check if the length of the combined data is less than 50
-                if (combinedData.length <= 100) {
-                    // Save combined data back to local storage
-                    localStorage.setItem('data', JSON.stringify(combinedData));
-                } else {
-                    console.log("Data limit reached. Not saving new predictions.");
-                }
-};
-
-
-    var prevTime;
-    var pastFrameTimes = [];
-    const detectFrame = function () {
-        if (!model) return requestAnimationFrame(detectFrame);
-
-        model
-            .detect(video)
-            .then(function (predictions) {
-                requestAnimationFrame(detectFrame);
-                renderPredictions(predictions);
-                
-            predictions.forEach(prediction => {
-                console.log("DETECTED", "Class: ", prediction.class, "Confidence: ", prediction.confidence, "Box: ", prediction.bbox);
-                });
-                
-                if (prevTime) {
-                    pastFrameTimes.push(Date.now() - prevTime);
-                    if (pastFrameTimes.length > 30) pastFrameTimes.shift();
-
-                    var total = 0;
-                    _.each(pastFrameTimes, function (t) {
-                        total += t / 1000;
-                    });
-                    
-                    var fps = pastFrameTimes.length / total;
-                    $("#fps").text(Math.round(fps));
-                }
-                prevTime = Date.now();
-            })
-            .catch(function (e) {
-                console.log("CAUGHT", e);
-                requestAnimationFrame(detectFrame);
-            });
-    };
-        // This should be done after you get the predictions
-    let results = predictions.map(prediction => ({
-        class: prediction.class,
-        confidence: Math.round(prediction.confidence * 100)
-    }));
-
-    localStorage.setItem('results', JSON.stringify(results));
+  localStorage.setItem("results", JSON.stringify(results));
 });
